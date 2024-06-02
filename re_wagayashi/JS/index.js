077334168fbbf2a928f4f03a0fa5eab15261afba
imataka <takashi6128_gakky@outlook.com>
2024-06-02 10:28:06 +0900

    .
    .

--- re_wagayashi/JS/index.js
+++ re_wagayashi/JS/index.js
@@ -1,33 +1,3 @@
-const COUNT = 10;
-const dots = Array.from({ length: COUNT });
-
-function animation() {
-  dots.map(function (dot, index) {
-    const angel = (360 / COUNT) * index;
-    return dot.animate(
-      [
-        {
-          transform: `rotate(${angle}deg) translateX(0px)`,
-          opacity: 1,
-        },
-        {
-          transform: `rotate(${angle}deg) translateX(100px)`,
-          opacity: 1,
-          offset: 0.8, // キーフレームの位置を80%の位置に設定
-        },
-        {
-          transform: `rotate(${angle}deg) translateX(100px)`,
-          opacity: 0,
-        },
-      ],
-      {
-        duration: 500,
-      }
-    );
-  });
-}
-
-
 const cart_btns = document.querySelectorAll(".cart_btn");
 const cart_cnt_icon = document.querySelector(".cart_cnt");
 let cart_cnt = 0;
@@ -77,3 +47,31 @@ cart_btns.forEach(function (cart_btn, index) {
 
 updateCart(); // カートの初期状態を反映
 
+// const COUNT = 10;
+// const dots = Array.from({ length: COUNT });
+
+// function animation() {
+//   dots.map(function (dot, index) {
+//     const angel = (360 / COUNT) * index;
+//     return dot.animate(
+//       [
+//         {
+//           transform: `rotate(${angle}deg) translateX(0px)`,
+//           opacity: 1,
+//         },
+//         {
+//           transform: `rotate(${angle}deg) translateX(100px)`,
+//           opacity: 1,
+//           offset: 0.8, // キーフレームの位置を80%の位置に設定
+//         },
+//         {
+//           transform: `rotate(${angle}deg) translateX(100px)`,
+//           opacity: 0,
+//         },
+//       ],
+//       {
+//         duration: 500,
+//       }
+//     );
+//   });
+// }
