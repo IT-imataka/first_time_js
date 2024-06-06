56a548f088ed486dd7dd892d4a4753c57c68708f
imataka <takashi6128_gakky@outlook.com>
2024-06-06 17:56:28 +0900

    weather_practice_twice(add error handling)
    天気API　2回目　エラーハンドリングたくさん追加した

--- practice2/script.js
+++ practice2/script.js
@@ -1,45 +1,52 @@
-document.addEventListener("DOMContentLoaded", function () {
+// const { response } = require("express");
+
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
-  const addbutton = document.getElementById("submit");
-  const userinput = document.getElementById("uservalue");
-  const lists = document.getElementById("lists");
+  const submit = document.getElementById("submit");
+  const vvvv = "WEATHER";
 
-  let todos = JSON.parse(localStorage.getItem("todos")) ?? [];
-
-  const loadtask = function (task, index) {
-    lists.innerHTML = "";
-    todos.forEach((task, index) => {
-      const li = document.createElement("li");
-      li.innerHTML = task;
-      const deletebutton = document.createElement("button");
-      deletebutton.innerText = "削除";
-      deletebutton.addEventListener("click", function () {
-        // li.remove();
-        deletetask();
-      });
-      li.appendChild(deletebutton);
-      lists.appendChild(li);
-    })
-  }
-
-  console.log(todos);
-
-  const addtask = function (event) {
-    const list = userinput.value.trim();
-    if (list) {
-      todos.push(list);
-      localStorage.setItem("todos", JSON.stringify(todos));
-      userinput.value = "";
-      loadtask();
-    }
+  const getweather = (event) => {
     event.preventDefault();
-  }
-  const deletetask = function (index) {
-    todos.splice(index, 1);
-    localStorage.setItem("todos", JSON.stringify(todos));
-    loadtask();
-  }
+    const usercity = document.getElementById("usercity").value;
+    const APIurl = `https://api.openweathermap.org/data/2.5/weather?q=${usercity}&appid=${vvvv}&units=metric&lang=ja`;
+    fetch(APIurl)
+      .then((res) => {
+        if (!res.ok) {
+          // console.error("正しい地域を入力してください。", (res) => {
+            window.alert("正しい地域を入力してください。",res);
+          // });
+        } else {
+          res.json().then((data) => {
+            console.log(data);
+            const location = data.name;
+            const temperature = data.main.temp;
+            const situation = data.weather[0].description;
+            const weather = data.weather[0].main;
 
-  form.addEventListener("submit", addtask);
-  loadtask();
+            if (usercity === location) {
+              document.getElementById(
+                "location"
+              ).innerHTML = `場所：${location}`;
+              document.getElementById(
+                "temperature"
+              ).innerHTML = `気温：${temperature}`;
+              document.getElementById(
+                "situation"
+              ).innerHTML = `天候状況：${situation}`;
+              document.getElementById("weather").innerHTML = `天候：${weather}`;
+            } else {
+              window.alert("都道府県名は日本語で入力してください");
+            }
+          });
+        }
+      })
+      .catch(function (error) {
+        console.error("Error", error);
+      });
+    displayweather();
+  };
+  const displayweather = () => {
+    usercity.value = "";
+  };
+  form.addEventListener("submit", getweather);
 });
